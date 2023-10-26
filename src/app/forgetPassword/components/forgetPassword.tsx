742b5ac59b7195966e596ca8be51ffb8e4333c9a
LuongMinhNhatSE160865 <105339044+LuongMinhNhatSE160865@users.noreply.github.com>
2023-10-26 17:10:35 +0700

    change forget pass, formik, axios

--- src/app/forgetPassword/components/forgetPassword.tsx
+++ src/app/forgetPassword/components/forgetPassword.tsx
@@ -1,34 +1,75 @@
-"use client"
+"use client";
 import React from "react";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button, Input, Spacer} from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Button,
+  Input,
+  Spacer,
+} from "@nextui-org/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function ForgetPassword() {
+  interface InitialValues {
+    email: string;
+  }
+
+  const initialValues: InitialValues = {
+    email:"",
+  }
+
+  const formik = useFormik (
+    {
+      initialValues,
+      validationSchema: Yup.object({
+        email: Yup.string().email("Wrong email format").required("Email can't be empty")
+
+      }),
+      onSubmit: async (values) =>{
+        console.log(values)
+      },
+    }
+  );
+
+
   return (
     <Card className="max-w-[600px] m-auto">
-      <CardHeader className="flex">
-        <div className="text-md font-bold m-auto text-3xl">
-          Forget Password
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody>
-      <div className="flex w-full flex-wrap md:flex-nowrap gap-4" color="success">
-      <Input type="email" label="Email" placeholder="Enter your email" />
-    </div>
-    <Spacer y={3}/>
-    <div className="flex m-auto">
-        <p className="text-sm color-warning">We will send you an Email for reset the password</p>
-    </div>
-      </CardBody>
-      <Divider/>
-      <CardFooter>
-        <Button radius="sm" type="submit" className="m-auto">
+      <form onSubmit={formik.handleSubmit}>
+        <CardHeader className="flex">
+          <div className="text-md font-bold m-auto text-3xl">
+            Forget Password
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div
+            className="flex w-full flex-wrap md:flex-nowrap gap-4"
+            color="success"
+          >
+            <Input id="email" label="Email" placeholder="Enter your email" onChange={formik.handleChange} isInvalid={!!formik.errors.email} errorMessage={formik.errors.email}/>
+          </div>
+          <div>
+            <p className=""></p>
+          </div>
+          <Spacer y={3} />
+          <div className="flex m-auto">
+            <p className="text-sm color-warning">
+              We will send you an Email for reset the password
+            </p>
+          </div>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Button radius="sm" type="submit" className="m-auto">
             {" "}
             Send{" "}
           </Button>
-      </CardFooter>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
-
-
--- src/app/userEdit/_components/UserEdit.tsx
+++ src/app/userEdit/_components/UserEdit.tsx
@@ -17,6 +17,9 @@ import CameraIcon from "./CameraIcon";
 import { useFormik } from "formik";
 import { storage } from "@/app/firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import * as Yup from "yup";
+
+
 
 export default function UserEdit() {
   interface InitialValues {
@@ -39,19 +42,21 @@ export default function UserEdit() {
 
   const formik = useFormik({
     initialValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email("Wrong email format").required("Email can't be empty"),
+      phonenumber: Yup.string().matches(/^\d{10}$/, 'Phone number must be 10 digits')
+      .required('Phone number is required'),
+    }),
     onSubmit: async (values) => {
       for (const image of values.images) {
         const storageRef = ref(storage, image.name);
-
         await uploadBytes(storageRef, image);
-
         const downloadUrl = await getDownloadURL(storageRef);
-
         console.log(downloadUrl);
       }
+      console.log(values)
     },
   });
-
 
   const [avatarSrc, setAvatarSrc] = useState(
     "https://i.pravatar.cc/150?u=a04258114e29026708c"
@@ -72,101 +77,99 @@ export default function UserEdit() {
     });
   }
 
-    return (
-      <Card className="max-w-[700px] mx-auto">
-        <form onSubmit={formik.handleSubmit}>
-          <CardHeader className="flex gap-3">
-            <div className="text-lg font-bold m-auto text-3xl">
-              Edit User Info
-            </div>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <div className="flex w-100 h-100 m-auto">
-              <Avatar
-                // src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-                src={avatarSrc}
-                className="w-55 h-55 m-auto static"
-                isBordered
-              />
-              <div className="flex items-center gap-4 m-auto mb-0">
-                <Button
-                  isIconOnly
-                  color="warning"
-                  variant="faded"
-                  aria-label="Take a photo"
-                  onClick={handleButtonClick}
-                >
-                  <CameraIcon />
-                </Button>
-              </div>
-            </div>
-            <Spacer y={4} />
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="text"
-                label="Full name"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <Spacer y={4} />
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="email"
-                label="Email"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <Spacer y={4} />
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="number"
-                label="Phone number"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <Spacer y={4} />
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="text"
-                label="Address"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <Spacer y={4} />
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="date"
-                label="Birthday"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <Spacer y={4} />
-            {/* <input onChange={async (event) => {
-            const files = event.target.files?.[0]
-          }}
-          /> */}
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <div className="flex m-auto item-center gap-7" color="active">
-              <Button radius="sm" type="submit" className="m-auto">
-                {" "}
-                Submit{" "}
-              </Button>
+  return (
+    <Card className="max-w-[700px] mx-auto">
+      <form onSubmit={formik.handleSubmit}>
+        <CardHeader className="flex gap-3">
+          <div className="text-lg font-bold m-auto text-3xl">
+            Edit User Info
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div className="flex w-100 h-100 m-auto">
+            <Avatar
+              id="images"
+              src={avatarSrc}
+              className="w-55 h-55 m-auto static"
+              isBordered
+            />
+            <div className="flex items-center gap-4 m-auto mb-0">
               <Button
-                radius="sm"
-                type="button"
-                className="m-auto"
-                color="danger"
+                isIconOnly
+                color="warning"
+                variant="faded"
+                aria-label="Take a photo"
+                onClick={handleButtonClick}
               >
-                {" "}
-                Cancel{" "}
+                <CameraIcon />
               </Button>
             </div>
-          </CardFooter>
-        </form>
-      </Card>
-    );
-  }
-
+          </div>
+          <Spacer y={4} />
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input
+              id="fullname"
+              type="text"
+              label="Full name"
+              onChange={formik.handleChange}
+              
+            />
+          </div>
+          <Spacer y={4} />
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input
+              id="email"
+              type="email"
+              label="Email"
+              onChange={formik.handleChange}
+              isInvalid={!!formik.errors.email}
+            />
+          </div>
+          <Spacer y={4} />
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input
+              id="phonenumber"
+              type="number"
+              label="Phone number"
+              onChange={formik.handleChange}
+              isInvalid={!!formik.errors.phonenumber}
+            />
+          </div>
+          <Spacer y={4} />
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input
+              id="address"
+              type="text"
+              label="Address"
+              onChange={formik.handleChange}
+            />
+          </div>
+          <Spacer y={4} />
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+            <Input
+              id="birthday"
+              type="date"
+              label="Birthday"
+              onChange={formik.handleChange}
+            />
+          </div>
+          <Spacer y={4} />
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <div className="flex m-auto item-center gap-7" color="active">
+            <Button radius="sm" type="submit" className="m-auto">
+              {" "}
+              Submit{" "}
+            </Button>
+            <Button radius="sm" type="button" className="m-auto" color="danger">
+              {" "}
+              Cancel{" "}
+            </Button>
+          </div>
+        </CardFooter>
+      </form>
+    </Card>
+  );
+}
